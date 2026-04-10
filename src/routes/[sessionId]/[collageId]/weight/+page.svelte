d711f3ad77745f2a32d03d224f7e9513b8f97ae4
Dmitry Konstantinov <konstantinov.dmitry@gmail.com>
2026-04-10 20:32:22 +0300

    weight diff badges

--- src/routes/[sessionId]/[collageId]/weight/+page.svelte
+++ src/routes/[sessionId]/[collageId]/weight/+page.svelte
@@ -11,7 +11,53 @@
 
 	let people = data.collage.data,
 		form,
+		prevItem,
+		diff = {},
+		first = {},
+		last = {},
+		globalDiff = {},
 		loading = false;
+
+	for (let i = 0; i < data.weights.length; i++) {
+		const item = data.weights[i].data;
+		const diffItem = {};
+
+		if (prevItem) {
+			Object.keys(item).forEach((name) => {
+				if (prevItem[name] && item[name]) {
+					diffItem[name] = prevItem[name] * 1000 - item[name] * 1000;
+				} else if (prevItem[name] && !item[name]) {
+					const prevValue = data.weights.slice(i).find(({ data }) => data[name])?.data?.[name];
+
+					if (prevValue) {
+						diffItem[name] = prevItem[name] * 1000 - prevValue * 1000;
+					}
+				}
+
+				if (item[name]) {
+					last[name] = item[name];
+				}
+			});
+
+			diff[data.weights[i - 1].date] = diffItem;
+		} else {
+			Object.keys(item).forEach((name) => {
+				if (!first[name] && item[name]) {
+					first[name] = item[name];
+				}
+
+				if (item[name]) {
+					last[name] = item[name];
+				}
+			});
+		}
+
+		prevItem = item;
+	}
+
+	people.forEach(({ name }) => {
+		globalDiff[name] = first[name] * 1000 - last[name] * 1000;
+	});
 </script>
 
 <div class="flex flex-col gap-3">
@@ -56,25 +102,41 @@
 	>
 	{#if data.weights.length > 0}
 		<h1 class="text-lg">История веса участников</h1>
-		<table class="table">
-			<thead>
-				<tr>
-					<th>Дата</th>
-					{#each people as p, i (i)}
-						<th>{p.name}</th>
-					{/each}
-				</tr>
-			</thead>
-			<tbody>
-				{#each data.weights as weight (weight.date)}
+		<div class="overflow-x-auto">
+			<table class="table">
+				<thead>
 					<tr>
-						<td>{format(new Date(weight.date), { noTime: true, noYear: true })}</td>
+						<th>Дата</th>
 						{#each people as p, i (i)}
-							<td>{weight.data[p.name]}</td>
+							<th>
+								{p.name}
+								{#if globalDiff[p.name] > 0}
+									<div class="badge badge-xs badge-error">+{globalDiff[p.name]}</div>
+								{:else if globalDiff[p.name] < 0}
+									<div class="badge badge-xs badge-success">{globalDiff[p.name]}</div>
+								{/if}
+							</th>
 						{/each}
 					</tr>
-				{/each}
-			</tbody>
-		</table>
+				</thead>
+				<tbody>
+					{#each data.weights as weight (weight.date)}
+						<tr>
+							<td>{format(new Date(weight.date), { noTime: true, noYear: true })}</td>
+							{#each people as p, i (i)}
+								<td>
+									{weight.data[p.name]}
+									{#if diff[weight.date]?.[p.name] > 0}
+										<div class="badge badge-xs badge-error">+{diff[weight.date][p.name]}</div>
+									{:else if diff[weight.date]?.[p.name] < 0}
+										<div class="badge badge-xs badge-success">{diff[weight.date][p.name]}</div>
+									{/if}
+								</td>
+							{/each}
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
 	{/if}
 </div>
